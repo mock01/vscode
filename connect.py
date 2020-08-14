a7331502feec7fae5046ce04d823a63e1a36cf42
mock01 <yan.ziqing@foxmail.com>
2020-08-14 13:40:25 +0800

    0814

---
+++ connect.py
@@ -0,0 +1,28 @@
+# -*-coding:utf-8 -*-
+import falcon
+from falcon import uri
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
+
+
+class Connect(object):
+
+    def on_get(self, req, resp):
+        query_string = req.query_string
+        query_list = query_string.split('&')
+        b = {}
+        for i in query_list:
+            b[i.split('=')[0]] = i.split('=')[1]
+
+        try:
+            check_signature(token='GpN3ws5zCdiwJlwXBq2uRe8h',
+                            signature=b['signature'], timestamp=b['timestamp'], nonce=b['nonce'])
+            resp.body = (b['echostr'])
+        except InvalidSignatureException:
+            pass
+        resp.status = falcon.HTTP_200
+
+
+app = falcon.API()
+connect = Connect()
+app.add_route('/connect', connect)
